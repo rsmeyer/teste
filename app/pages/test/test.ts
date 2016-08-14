20d52f7199d19a33dfe5c90f262adf11ecfbb203
Rafael Meyer <meyer.linux@gmail.com>
2016-08-14 11:24:09 -0300

    14-08-16 v3

---
+++ app/pages/test/test.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the TestPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/test/test.html',
+})
+export class TestPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
