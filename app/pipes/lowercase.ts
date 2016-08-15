a2d8b656161717c641c0ffe6763dcd73f5ebb224
Rafael Meyer <meyer.linux@gmail.com>
2016-08-15 08:30:02 -0300

    15-08-16 v1

---
+++ app/pipes/lowercase.ts
@@ -0,0 +1,21 @@
+import { Injectable, Pipe } from '@angular/core';
+
+/*
+  Generated class for the Lowercase pipe.
+
+  See https://angular.io/docs/ts/latest/guide/pipes.html for more info on
+  Angular 2 Pipes.
+*/
+@Pipe({
+  name: 'lowercase'
+})
+@Injectable()
+export class Lowercase {
+  /*
+    Takes a value and makes it lowercase.
+   */
+  transform(value: string, args: any[]) {
+    value = value + ''; // make sure it's a string
+    return value.toLowerCase();
+  }
+}
